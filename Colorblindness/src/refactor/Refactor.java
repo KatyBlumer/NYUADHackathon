a1e697222c8be8e0a2cadb87c3ac8c658d8a492d
Seung <smo304@nyu.edu>
2013-02-23 05:07:25 -0500

    addition to refactor
    and camerathing

--- Colorblindness/src/refactor/Refactor.java
+++ Colorblindness/src/refactor/Refactor.java
@@ -8,54 +8,49 @@ enum color{Red,Orange,Yellow,Green,Cyan,Blue,Violet,Purple,none}
 
 public class Refactor {
 
-	
-	
 		color colorguesser(float [] hsv,int position)
 		{
-			
-					if(hsv[position]<=15 || hsv[position]>=316)
-					{	
-						return color.Red;
-					}
-					else if(hsv[position]>=16 && hsv[position]<=45)
-					{
-						return color.Orange;
-					}else if(hsv[position]>=46 && hsv[position]<=75)
-					{
-						return color.Yellow;
-					}else if(hsv[position]>=76 && hsv[position]<=165)
-					{
-						return color.Green;
-					}else if(hsv[position]>=166 && hsv[position]<=195)
-					{
-						return color.Cyan;
-					}else if(hsv[position]>=196 && hsv[position]<=255)
-					{
-						return color.Blue;
-						
-					}else if(hsv[position]>=256 && hsv[position]<=285)
-					{
-						return color.Violet;
-						
-					}else if(hsv[position]>=286 && hsv[position]<=315)
-					{
-						return color.Purple;
-					}
-			    	
-			       return color.none;
-			    }
-			    
-	   
-			
-		
-		
+				if(hsv[position]<=15 || hsv[position]>=316)
+				{	
+					return color.Red;
+				}
+				else if(hsv[position]>=16 && hsv[position]<=45)
+				{
+					return color.Orange;
+					
+				}else if(hsv[position]>=46 && hsv[position]<=75)
+				{
+					return color.Yellow;
+					
+				}else if(hsv[position]>=76 && hsv[position]<=165)
+				{
+					return color.Green;
+					
+				}else if(hsv[position]>=166 && hsv[position]<=195)
+				{
+					return color.Cyan;
+					
+				}else if(hsv[position]>=196 && hsv[position]<=255)
+				{
+					return color.Blue;
+					
+				}else if(hsv[position]>=256 && hsv[position]<=285)
+				{
+					return color.Violet;
+					
+				}else if(hsv[position]>=286 && hsv[position]<=315)
+				{
+					return color.Purple;
+				}
+		    	
+		       return color.none;
+		    }
+		    
 		byte [] covertfrombitmaptobytearray(Bitmap bitmap)
 		{
 			ByteBuffer buffer = ByteBuffer.allocate(4 * bitmap.getWidth() * bitmap.getHeight());
 			 bitmap.copyPixelsToBuffer(buffer); 
 			 return buffer.array();
-			
-			
 		}
 		
 		float [] covertfromrgbtohsv(byte [] imagebyte)
@@ -75,6 +70,27 @@ public class Refactor {
 			return hsvarray;
 		}
 		
-		
+		float[] change(Bitmap bitmap,int mode)
+		{
+			
+			float[] hsv=covertfromrgbtohsv(covertfrombitmaptobytearray(bitmap));
+			float[] res=new float[hsv.length];
+			for(int i=0;i<hsv.length;i+=3)
+			{
+				color c=colorguesser(hsv,i);
+				switch(c)
+				{
+				case Red:
+				case Orange:
+				case Yellow:
+				case Green:
+				case Cyan:
+				case Blue:
+				case Violet:
+				case Purple:
+				}
+			}
+			return res;
+		}
 	
 }
