d55ea755ff106f55ee02a2939ae0a28bae0896d3
Akram Dweikat <Akram.Dweikat@hotmail.com>
2013-02-23 12:58:58 +0200

    layout update

--- Colorblindness/gen/com/adventureteam/colorblindness/R.java
+++ Colorblindness/gen/com/adventureteam/colorblindness/R.java
@@ -28,11 +28,13 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int dummy_button=0x7f070002;
         public static final int fullscreen_content=0x7f070000;
         public static final int fullscreen_content_controls=0x7f070001;
+        public static final int textView1=0x7f070003;
     }
     public static final class layout {
         public static final int activity_start_screen=0x7f030000;
     }
     public static final class string {
+        public static final int ColorType=0x7f050004;
         public static final int app_name=0x7f050000;
         public static final int dummy_button=0x7f050002;
         public static final int dummy_content=0x7f050003;
